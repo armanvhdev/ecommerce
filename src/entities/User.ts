3af405a23f722fa709effaab46cedf0d66892d73
arman <armanvh181@gmail.com>
2023-04-25 23:27:19 +0430

    fixed data type bug

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,4 +1,11 @@
-import { Entity, Column, PrimaryGeneratedColumn, BaseEntity } from 'typeorm';
+import {
+   Entity,
+   Column,
+   PrimaryGeneratedColumn,
+   BaseEntity,
+   CreateDateColumn,
+   UpdateDateColumn,
+} from 'typeorm';
 import {
    IsNotEmpty,
    IsString,
@@ -18,25 +25,31 @@ export class User extends BaseEntity {
    @PrimaryGeneratedColumn()
    id: number;
 
-   @Column({ type: 'string', length: 50 })
+   @Column({ length: 50 })
    @IsNotEmpty({ message: 'نام نمیتواند خالی باشد' })
    @IsString({ message: 'لطفامقدار درست راوارد کنید' })
    @MinLength(3, { message: 'تعداد حروف نام شما نباید کمتر از 3 باشد' })
    @MaxLength(50, { message: 'تعداد حروف نام نباید بیشتر از 50 باشد' })
    name: string;
 
-   @Column({ type: 'string', length: 50 })
+   @Column({ length: 50 })
    @IsNotEmpty({ message: 'رمز عبور نمیتواند خالی باشد' })
    @IsString({ message: 'لطفامقدار درست راوارد کنید' })
    @MinLength(6, { message: 'تعداد حروف رمز عبور نباید کمتر از 6 باشد' })
    password: string;
 
-   @Column()
+   @Column({ unique: true })
    @IsNotEmpty({ message: 'ایمیل نمیتواند خالی باشد' })
    @IsEmail()
    email: string;
 
-   @Column({ type: 'enum', default: 'user' })
+   @Column({ type: 'enum', enum: Role, default: 'user' })
    @IsEnum(Role)
-   role: Role;
+   role: string;
+
+   @CreateDateColumn()
+   createdAt: Date;
+
+   @UpdateDateColumn()
+   updatedAt: Date;
 }
