b717c71553351b8a3890e80f746e6f8ed22c6047
arman <armanvh181@gmail.com>
2023-05-23 19:36:33 +0330

    authentication controller

--- src/controller/authController.ts
+++ src/controller/authController.ts
@@ -3,7 +3,7 @@ import { User } from '../entities/User';
 import { generateHash } from '../utils/password-hash';
 import { StatusCodes } from 'http-status-codes';
 import CustomError from '../errors';
-import { attachCookiesToResponse } from '../utils';
+import { attachCookiesToResponse, compareHash } from '../utils';
 
 const register = async (req: Request, res: Response): Promise<void> => {
    const { email, name, password } = req.body;
@@ -31,11 +31,33 @@ const register = async (req: Request, res: Response): Promise<void> => {
 };
 
 const login = async (req: Request, res: Response) => {
-   res.send('login user');
+   const { email, password } = req.body;
+   if (!email || !password) {
+      throw new CustomError.BadRequsetError(
+         'please provide email and password'
+      );
+   }
+   const user = await User.findOneBy({ email });
+   if (!user) {
+      throw new CustomError.UnAuthenticatedError('invalid credetials');
+   }
+
+   if (!compareHash(password, user.password)) {
+      throw new CustomError.UnAuthenticatedError('invalid credetials');
+   }
+
+   const tokenUser = { name: user.name, userId: user.id, role: user.role };
+   attachCookiesToResponse({ res, user: tokenUser });
+
+   res.status(StatusCodes.OK).json(tokenUser);
 };
 
 const logout = async (req: Request, res: Response) => {
-   res.send('logout user');
+   res.cookie('token', 'logout', {
+      httpOnly: true,
+      expires: new Date(Date.now()),
+   });
+   res.status(StatusCodes.OK).json({ msg: 'user logged out!' });
 };
 
 export { register, login, logout };
